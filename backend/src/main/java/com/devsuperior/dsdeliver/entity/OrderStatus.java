8ee20515836a45f1c71ac7a70ec5930ea4cb98ce
paulaleticia21 <paula.leticia.braz@gmail.com>
2021-01-07 13:08:18 -0300

    Domain Model

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/entity/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dsdeliver.entity;
+
+public enum OrderStatus {
+
+	// TRADUÇÃO DE CODIGO , O PE É ( ZERO) E O DE É ( UM) É UM TIPO NUMERADO 
+	PENDING, DELIVERED;
+	
+}
---
+++ backend/src/main/java/com/devsuperior/dsdeliver/entity/Product.java
@@ -0,0 +1,106 @@
+package com.devsuperior.dsdeliver.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+// aqui os produtos corresponde uma entidade de dominio
+@Entity
+@Table(name = "tb_product")
+
+public class Product implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	// fazer um mapeamento de uma chave primaria no banco
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Double price;
+	private String description;
+	private String imageUri;
+
+	// Construtor vazio
+	public Product() {
+
+	}
+
+	public Product(Long id, String name, Double price, String description, String imageUri) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.description = description;
+		this.imageUri = imageUri;
+	}
+	// Get e set
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImageUri() {
+		return imageUri;
+	}
+
+	public void setImageUri(String imageUri) {
+		this.imageUri = imageUri;
+	}
+
+	// fazer uma comparação de um objeto com o outro , essa comparação esta sendo
+	// pelo ID
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
