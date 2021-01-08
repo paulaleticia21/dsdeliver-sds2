b9a5bba57591d3c3245a90699b6e70b15740c5b0
paulaleticia21 <paula.leticia.braz@gmail.com>
2021-01-07 23:01:46 -0300

    Ready to prod

--- backend/src/main/java/com/devsuperior/dsdeliver/entity/Product.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entity/Product.java
@@ -11,7 +11,6 @@ import javax.persistence.Table;
 // aqui os produtos corresponde uma entidade de dominio
 @Entity
 @Table(name = "tb_product")
-
 public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -85,22 +84,27 @@ public class Product implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Product other = (Product) obj;
-		if (id != other.id)
-			return false;
-		return true;
-	}
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Product other = (Product) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+
 
 }
